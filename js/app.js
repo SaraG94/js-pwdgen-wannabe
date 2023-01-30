07db4ecdd51c1e4af7600697e701ff0b1b2b37be
SaraG94 <sara.gherbassi.94@gmail.com>
2023-01-30 18:04:03 +0100

    FEAT strings add

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 //prendere elemento p
-let pElement =document.getElementById('your-pw')
+let pElement =document.getElementById('your-pw');
 
 //chiedere all'utente nome
-let firstname = prompt('Ciao! Aiutami a generare una nuova password. Come ti chiami?')
+let firstname = prompt('Ciao! Aiutami a generare una nuova password. Come ti chiami?');
 //chiedere cognome
-let lastname = prompt('Qual è il tuo Cognome?')
+let lastname = prompt('Qual è il tuo Cognome?');
 //chiedere colore preferito 
-let color = prompt('Qual è il tuo colore preferito?')
+let color = prompt('Qual è il tuo colore preferito?');
 //chiedere numero
-let luckynumber = prompt('E il tuo numero preferito?')
-//sommare stringhe
-//stampare password
+let luckynumber = prompt('E il tuo numero preferito?');
+//sommare stringhe e stampare password al posto di p
+pElement.innerHTML = firstname + color + luckynumber + lastname;
